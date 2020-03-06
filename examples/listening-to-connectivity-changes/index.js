fa7a11f582be8fb7b208232b0c71a0c054e46812
Halim Qarroum <hqm.post@gmail.com>
2020-03-06 20:57:51 +0100

    Bump version

--- examples/listening-to-connectivity-changes/index.js
+++ examples/listening-to-connectivity-changes/index.js
@@ -5,9 +5,5 @@ const emitter = new IsOnlineEmitter();
 
 // Listening to `connectivity.change` events.
 emitter.on('connectivity.change', console.log);
-// Listening to `connectivity.check.scheduled` events.
-emitter.on('connectivity.check.scheduled', (delay) => {
-  console.log(`Next Internet connectivity event is scheduled in '${delay}ms'.`);
-});
 // Starting the connectivity monitoring.
 emitter.start();
